2085d7a12284a6a8706cc65b0f8a8dbf412202e1
Sakthivel S <srsakthivel531@gmail.com>
2026-02-03 18:36:57 +0530

    Create full_addr_dataflow.v

---
+++ COMBINATIONAL _ckt 1/ADDERS/full_addr_dataflow.v
@@ -0,0 +1,28 @@
+//design file 
+module full_adder_data(input a,b,c,output sum,cout);
+  assign sum=a^b^c;
+  assign cout=a&b;
+endmodule 
+
+
+//test bench file 
+module full_adder_data_tb;
+  reg a,b,c;
+  wire sum,cout;
+  full_adder_data uut(a,b,c,sum,cout);
+  initial 
+    begin
+      $monitor ("time=%0t a=%b b=%b  c=%d sum=%b cout=%b ", $time,a,b,c,sum,cout);
+      a=0;b=0;c=0;
+      #5 a=0;b=0;c=1;
+      #5 a=1;b=1;c=0;
+      #5 a=0;b=1;c=1;
+      #5 a=1;b=0;c=0;
+      #5 a=1;b=0;c=1;
+      #5 a=1;b=1;c=0;
+      #5 a=1;b=1;c=1;
+      #80 $finish;
+      $dumpfile("full_adder_data.vcd");   
+      $dumpvars(0, full_adder_data_tb); 
+    end 
+endmodule
