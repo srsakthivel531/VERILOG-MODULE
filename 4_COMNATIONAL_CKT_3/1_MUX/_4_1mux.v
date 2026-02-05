b836bdd585e51aaa4005c8695fd5d15f9fb2d372
Sakthivel S <srsakthivel531@gmail.com>
2026-02-05 09:27:50 +0530

    Update _4_1mux.v

--- 4_COMNATIONAL_CKT_3/1_MUX/_4_1mux.v
+++ 4_COMNATIONAL_CKT_3/1_MUX/_4_1mux.v
@@ -1,5 +1,5 @@
 //design file 
-module _4_1_data(input [1:0]a,b,c,d,s1,s0,output [1:0]y);
+module _4_1_data(input [1:0]a,b,c,d,input s1,s0,output [1:0]y);
   assign y=s1?(s0?d:c):(s0?b:a);
 endmodule 
 
