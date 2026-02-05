9d1413de4d9bfda4d9dc06eea27b858335e2d08e
Sakthivel S <srsakthivel531@gmail.com>
2026-02-05 09:24:03 +0530

    Update _4_1mux.v

--- 4_COMNATIONAL_CKT_3/1_MUX/_4_1mux.v
+++ 4_COMNATIONAL_CKT_3/1_MUX/_4_1mux.v
@@ -1,17 +1,18 @@
 //design file 
-module _4_1_data(input a,b,c,d,s1,s0,output y);
+module _4_1_data(input [1:0]a,b,c,d,s1,s0,output [1:0]y);
   assign y=s1?(s0?d:c):(s0?b:a);
 endmodule 
 
 //test bench file 
 module _4_1_data_tb;
-  reg a,b,c,d,s0,s1;
+  reg [1:0]a,b,c,d;
+  reg s0,s1;
   wire y;
   _4_1_data uut(a,b,c,d,s0,s1,y);
   initial 
     begin 
-      $monitor("time=%0t a=%b b=%b s0=%b s1=%b y=%b",$time,a,b,s0,s1,y);
-      a=0;b=1;c=1;d=1;
+      $monitor("time=%0t s0=%b s1=%b y=%b",$time,s0,s1,y);
+      a=2'b00;b=2'b01;c=2'b10;d=2'b11;
       #3 s1=1;s0=1;
        #3 s1=0;s0=1;
        #3 s1=0;s0=0;
