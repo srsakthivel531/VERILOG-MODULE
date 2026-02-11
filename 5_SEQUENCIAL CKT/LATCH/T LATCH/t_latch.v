ad38b2bd2f939783500b08e1e21a4bacf6ca7a87
Sakthivel S <srsakthivel531@gmail.com>
2026-02-11 22:18:09 +0530

    Update t_latch.v

--- 5_SEQUENCIAL CKT/LATCH/T LATCH/t_latch.v
+++ 5_SEQUENCIAL CKT/LATCH/T LATCH/t_latch.v
@@ -1,26 +1,28 @@
 //design file
-module t_latch(input t,clk,output reg q);
+module t_latch(input t,clk,input rst,output reg q);
   always@(clk or t)
     begin 
+      if(rst)
       q<=1'b0;
-      if(clk && t)
+      else if(clk && t)
       q<=~q;
      end 
 endmodule 
 
 //test bench file 
 module t_latch_tb;
-  reg t,clk;
+  reg t,clk,rst;
   wire  q;
-  t_latch uut(t,clk,q);
+  t_latch uut(t,clk,rst,q);
   initial begin 
      clk=1'b0;
   
     forever #5 clk=~clk;
   end 
   initial begin
-    $monitor("$time=%0t t=%b clk=%b q=%b",$time,t,clk,q);
-     t=1;
+    $monitor("$time=%0t t=%b clk=%b rst=%b q=%b",$time,t,clk,rst,q);
+     t=1;rst=1;
+   #2 rst=0;
     #5 t=1;
     #4 t=0;
     #4 t=1;
@@ -33,4 +35,3 @@ module t_latch_tb;
     $dumpfile("t_latch.vcd");
     $dumpvars(0,t_latch_tb);
     end 
-endmodule 
